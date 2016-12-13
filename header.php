ecc6015462ecce32f6dd32feb0d6a1dbaa3a89e4
simonsmh <simonsmh@gmail.com>
2016-12-13 22:34:24 +0800

    refine icon fonts See https://material.io/icons/ for more usage Say goodby to glyphicons

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 			</div>
 			<div class="navbar-collapse collapse navbar-responsive-collapse">
 				<ul class="nav navbar-nav">
-					<li <?php if($this->is('index')): ?>class="active"<?php endif; ?>><a href="<?php $this->options->siteUrl(); ?>"><span class="glyphicon glyphicon-fire" aria-hidden="true"></span> <?php $this->options->title() ?></a></li>
+					<li <?php if($this->is('index')): ?>class="active"<?php endif; ?>><a href="<?php $this->options->siteUrl(); ?>"><span class="mdi-action-explore md-12"></span> <?php $this->options->title() ?></a></li>
 					<?php $this->widget('Widget_Metas_Category_List')->to($category); ?>
 					<?php while($category->next()): ?>
 					<?php if(count($category->children)):?>
